9fd5ea3693f6b832eaa0a9cb37061bab117bbf8d
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-09-11 18:37:55 +0200

    annotate fig and use fewer iterations

--- 4. Tapping Estimates.R
+++ 4. Tapping Estimates.R
@@ -1,3 +1,4 @@
+library(glue)
 library(dplyr)
 library(magrittr)
 library(tidyr)
@@ -8,7 +9,8 @@ v <- sampler_mc3(
   start = rnorm(1, .5), 
   distr_name = "norm", 
   distr_params = c(3, 5), 
-  sigma_prop = 1
+  sigma_prop = 1, 
+  iterations = 500
 )$Samples[,,1]
 psd <- samplr::calc_PSD(v, plot = F)
 tibble(
@@ -18,10 +20,12 @@ tibble(
   ggplot(aes(lf, lp)) + 
   geom_line() + 
   geom_abline(intercept = psd$polyfit[2], slope = psd$polyfit[1], color="blue") + 
-  geom_segment(x=-2.5, y=-1, xend=-1.5, yend=-1 - 1) + 
-  geom_segment(x=-2.5, y=-1, xend=-1.5, yend=-1 - .5) + 
-  geom_segment(x=-2.5, y=-1, xend=-1.5, yend=-1 - 1.5) + 
-  geom_segment(x=-2.5, y=-1, xend=-1.5, yend=-1 - .75) + 
-  geom_segment(x=-2.5, y=-1, xend=-1.5, yend=-1 - 1.25) + 
-  annotate("text", x = -2, y = -1, label="1/f", size=8)
-
+  geom_segment(x=-2.5, y=0, xend=-1.5, yend=- 1) + 
+  geom_segment(x=-2.5, y=0, xend=-1.5, yend=- .5) + 
+  geom_segment(x=-2.5, y=0, xend=-1.5, yend=- 1.5) + 
+  geom_segment(x=-2.5, y=0, xend=-1.5, yend=- .75) + 
+  geom_segment(x=-2.5, y=0, xend=-1.5, yend=- 1.25) + 
+  annotate("text", x = -2, y = -1, label="1/f", size=8) + 
+  annotate("text", x = -1, y = 2.5, 
+           label=glue("alpha == {round(- psd$polyfit[1], 2)}"), 
+           size=8, color="blue", parse=T) 
