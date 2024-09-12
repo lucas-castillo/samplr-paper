96c12d6ccd0b8512015b779b2106c53ccc414de0
Yun-Xiao Li <yunxiao.li@outlook.com>
2024-09-12 14:24:39 +0100

    Change to the boxplot in the over-precision figure

--- 6. Over-precision.R
+++ 6. Over-precision.R
@@ -4,12 +4,12 @@ library(ggplot2)
 library(dplyr)
 set.seed(2024)
 
-trial_stim <- round(runif(1, 10, 50))
-all <- tibble()
+df_conf <- tibble()
 for (i in 1:100){
+  trial_stim <- round(runif(1, 10, 50)) # inside or outside?
   abs_model <- Zhu23ABS$new(width=2, n_chains=8, nd_time=0.4,
                             s_nd_time=0.3, lambda = 100, distr_name = 'norm', distr_params = 1)
-  abs_model$simulate(stopping_rule = 'fixed', n_sample = 5, trial_stim = trial_stim)
+  abs_model$simulate(stopping_rule = 'fixed', n_sample = 10, trial_stim = trial_stim)
   sub_prob <- seq(0.5, 1, by=0.1)
   proportion <- matrix(0, 1, 6)
   
@@ -20,15 +20,23 @@ for (i in 1:100){
     ub <- conf_interval[2]
     return(pnorm(ub, mean=trial_stim, sd=1) - pnorm(lb, mean=trial_stim, sd=1))
   })
-  all <- rbind(all, tibble(sub_prob, proportion, i))  
+  df_conf <- rbind(df_conf, tibble(sub_prob, proportion, i))
 }
 
+df_conf_summary <- df_conf %>%
+  group_by(sub_prob) %>%
+  summarise(mean_prop = mean(proportion),
+            se = sd(proportion)/sqrt(n()))
 
-fig <- all %>%
-  ggplot(aes(x=sub_prob, y=proportion)) +
+
+fig <- df_conf %>%
+  ggplot(aes(x=sub_prob, y=proportion, group = sub_prob)) +
+  geom_boxplot() +
   geom_abline(slope = 1, linetype = 'dashed') +
-  geom_jitter(width=.01) +
-  scale_x_continuous(limits = c(0, 1)) +
-  scale_y_continuous(limits = c(0, 1)) +
+  geom_jitter(alpha = 0.5, shape=16, position=position_jitter(0.01))+
+  scale_x_continuous(limits = c(0, 1.1),
+                     breaks = seq(0, 1, by=0.2),
+                     labels = seq(0, 1, by=0.2)) +
+  scale_y_continuous(limits = c(0, 1.1)) +
   labs(x = 'Subjective probability', y = 'Proportion')
 fig
