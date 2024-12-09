e92361ee3c10c22cb70281778866fbd1126ee3a6
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-12-09 11:00:57 +0000

    make figure like in Sundh et al 2023 (first attempt)

--- 1. Probability Judgments.R
+++ 1. Probability Judgments.R
@@ -5,6 +5,7 @@ library(magrittr)
 library(tidyr)
 library(ggplot2)
 library(samplr)
+library(patchwork)
 source("src/theme.R")
 set.seed(123)
 
@@ -18,12 +19,12 @@ b_and_not_a = b * not_a
 a_and_not_b = a * not_b
 not_a_and_not_b = 1 - a_and_b - b_and_not_a - a_and_not_b
 beta <- .25
-
+Ns <- c(1,3,7, 20)
 ## data points -- 
 theoretical <- tibble()
 simulated <- tibble()
 
-for (N in c(1, 5, 20)){
+for (N in Ns){
   mean_estimates <- Bayesian_Sampler(
     a_and_b = a * b,
     b_and_not_a = b * not_a,
@@ -54,11 +55,12 @@ for (N in c(1, 5, 20)){
   
   theoretical <- rbind(
     theoretical, 
-    tibble(M=mean_estimates, V=var_estimates, beta=beta, N=N)
+    tibble(true=a, M=mean_estimates, V=var_estimates, beta=beta, N=N)
   )
   simulated <- rbind(
     simulated,
     tibble(
+      true=a,
       M=simulations %>% 
         apply(2, mean),
       V=simulations %>% 
@@ -68,32 +70,71 @@ for (N in c(1, 5, 20)){
     )  
   )    
 }
-
-## figure -- 
+# make PT + N
+ptn <- expand_grid(true = a, N=Ns) %>% 
+  mutate(d = ifelse(N==1, .17, ifelse(N==3, .07, ifelse(N==7, .03, .01)))) %>% 
+  mutate(exp = (1 - 2*d) * true + d) %>% 
+  mutate(var = (1/N) * (exp * (1 - exp))) %>% 
+  # mutate(across(c(d, N), factor)) %>% 
+  mutate(label = glue("N = {N}; d = {d}")) %>% 
+  mutate(label = forcats::fct_reorder(label, N))
+# finalize BS dfs
 theoretical <- theoretical %>% 
-  mutate(across(c(beta, N), factor)) %>% 
-  nest(.by=beta) %>% 
-  rowwise() %>% 
-  mutate(beta_label = TeX(glue("$\\beta={beta}$"), output = "character")) %>% 
+  nest(.by=c(beta, N)) %>% 
+  rowwise() %>%
+  mutate(label = glue("N = {N}; $\\beta={beta}$")) %>% 
+  mutate(label = forcats::fct_reorder(label, N)) %>% 
   unnest(data)
 simulated <- simulated %>% 
-  mutate(across(c(beta, N), factor)) %>% 
-  nest(.by=beta) %>% 
-  rowwise() %>% 
-  mutate(beta_label = TeX(glue("$\\beta={beta}$"), output = "character")) %>% 
+  nest(.by=c(beta, N)) %>% 
+  rowwise()  %>%
+  mutate(label = glue("N = {N}; $\\beta={beta}$")) %>% 
+  mutate(label = forcats::fct_reorder(label, N)) %>% 
   unnest(data)
 
 
-simulated %>% 
-  ggplot(aes(M, V,color=N, fill=N,shape=N, linetype = N)) + 
-  geom_point(size=1.1, color="black", alpha=.8) + 
-  geom_line(data=theoretical) +
-  facet_grid(cols = vars(beta_label), labeller = label_parsed) + 
+## figure -- 
+
+p1 <- ptn %>% 
+  ggplot(aes(true, exp, color=label)) + 
+  geom_line() + 
+  xlab("P(A)") + 
+  ylab("Model Prediction") + 
+  theme(legend.position = c(.2, .8))
+
+p2 <- theoretical %>% 
+  ggplot(aes(true, M, color=label)) +
+  geom_line() + 
+  xlab("P(A)") + 
+  ylab("Model Prediction") + 
+  theme(legend.position = c(.2, .8))
+
+p3 <- ptn %>% 
+  ggplot(aes(exp, var, color=label, linetype=label, group=label)) +
+  geom_line() +
   xlab("Mean") +
   ylab("Variance") +
-  scale_shape_manual(values=21:23) +
-  scale_linetype_manual(values=c(1,2,5)) + 
+  scale_shape_manual(values=21:24) +
+  scale_linetype_manual(values=c(1,2,5, 6)) + 
   scale_fill_brewer(palette="Set1") + 
-  scale_x_continuous(labels = \(x){x})
+  scale_color_brewer(palette="Set1") + 
+  scale_x_continuous(labels = \(x){x}) + 
+  theme(legend.position = c(.2, .8))
+
+
+p4 <- simulated %>% 
+  ggplot(aes(M, V, color= label, fill=label,shape=label, linetype = label, group=label)) + 
+  geom_point(size=1.1, alpha=.8) + 
+  geom_line(data=theoretical) +
+  xlab("Mean") +
+  ylab("Variance") +
+  scale_shape_manual(values=21:24) +
+  scale_linetype_manual(values=c(1,2,5, 6)) + 
+  scale_fill_brewer(palette="Set1") + 
+  scale_color_brewer(palette="Set1") + 
+  scale_x_continuous(labels = \(x){x}) + 
+  theme(legend.position = c(.2, .8))
+
+(p1 + p2) / (p3 + p4) + plot_layout(guides="collect")
 
 ggsave("plots/probability_judgments.png", width = w, height = w/2, dpi = 300)
