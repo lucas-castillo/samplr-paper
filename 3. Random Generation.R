1d8792d89c5505dbb1c98d8f8ba6b24712b07e02
Lucas Castillo <lucas.castillo.marti@gmail.com>
2026-08-25 17:36:46 +0100

    change fig 4 to show prior predictives of multiple models

--- 3. Random Generation.R
+++ 3. Random Generation.R
@@ -8,6 +8,8 @@ library(samplrData)
 set.seed(2024)
 source("src/rg_functions.R")
 source("src/theme.R")
+models <- c("MH", "MC3", "HMC", "MCREC")
+
 
 empirical <- samplrData::castillo2024.rgmomentum.e1 %>% 
   mutate(value = ifelse(value < 100, NA, value)) %>% 
@@ -15,21 +17,25 @@ empirical <- samplrData::castillo2024.rgmomentum.e1 %>%
 
 simulations <- tibble()
 for (i in 1:100){
-  simulations <- rbind(
-    simulations,
-    get_measures(simulate("MCREC", prior(), "norm")) %>% mutate(target="N"),
-    get_measures(simulate("MCREC", prior(), "unif")) %>% mutate(target="U")
-    )
+  for (model in models){
+    simulations <- rbind(
+      simulations,
+      get_measures(simulate(model, prior(), "norm")) %>% mutate(model=model, target="N"),
+      get_measures(simulate(model, prior(), "unif")) %>% mutate(model=model, target="U")
+      )
+  }
 }
+
 sum_simulations <- simulations %>% 
   pivot_longer(R:S) %>% 
   mutate(name = factor(name, levels=c("R", "A", "TP", "D", "S"))) %>%
-  group_by(target, name) %>% 
+  group_by(model, target, name) %>% 
   summarise(M=mean(value), S=sd(value)) %>% 
   rename(target_dist = "target") %>% 
   mutate(target_dist = factor(target_dist)) %>% 
-  mutate(target_distN = as.numeric(target_dist))
-
+  mutate(target_distN = as.numeric(target_dist)) %>% 
+  ungroup %>% 
+  mutate(model = factor(model, level=models))
 
 measure_names <- c(
   "R"="Repetitions",
@@ -58,11 +64,11 @@ empirical %>%
   geom_boxplot(aes(target_dist, value)) + 
   facet_wrap(vars(name), scales = "free", ncol = 1) +
   geom_rect(
-    mapping = aes(xmin = target_distN-.25, xmax = target_distN+.25, ymin = M - .5*S, ymax=M+.5*S),
+    mapping = aes(xmin = target_distN-.45, xmax = target_distN+.45, ymin = M - .5*S, ymax=M+.5*S, fill=model),
     data = sum_simulations %>% 
       rowwise() %>% 
       mutate(name = factor(measure_names[name], levels=measure_names)),# %>% filter(name == "R"),
-    fill="grey", linewidth=.5, alpha=.5, color="black"
+    linewidth=.5, color="transparent", alpha=.75, position = "dodge"
   ) + 
   coord_flip() + 
   xlab("Condition") + 
@@ -70,6 +76,6 @@ empirical %>%
     axis.title.x = element_blank(),
     # axis.title.y = element_blank(),
     # axis.text = element_blank(),
-  )
-
-ggsave("plots/random_generation.png", width = w, height = w/1.44, dpi = 300)
+  ) + 
+  guides(fill=guide_legend(title="Model"))
+ggsave("plots/random_generation.png", width = w, height = w/1.3, dpi = 300)
